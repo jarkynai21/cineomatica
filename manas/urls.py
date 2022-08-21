844941030929f133edc4cbb627c57691ebcc36dc
jarkynai <jarkynai715@gmail.com>
2022-08-22 00:26:30 +0600

    git

--- manas/urls.py
+++ manas/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('broadway.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/auth/', include('djoser.urls')),
+    path(r'^auth/', include('djoser.urls.authtoken')),
 
 ]
